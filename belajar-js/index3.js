19eed0926dc1156b7a6443d26e39d8b886ed89fa
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2022-06-16 20:32:00 +0700

    challenge-3

---
+++ belajar-js/index3.js
@@ -0,0 +1,28 @@
+//for
+for(let i = 0; i < 5; i++){
+	console.log(i)
+}
+console.log('===============')
+
+let myArray = ['Luke', 'Han', 'Chw', 'Kia']
+
+for (a of myArray){
+	console.log(a)
+}
+console.log('===============')
+
+//While
+let b = 1;
+while(b <= 100){
+	console.log(b)
+	b++;
+}
+console.log('===============')
+
+//Object
+const user = {
+	key1: 'value1',
+	key2: 'value2',
+}
+
+console.log(`Halo, nama saya ${user.key1}`)

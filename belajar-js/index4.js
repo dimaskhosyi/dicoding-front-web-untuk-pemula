1e0a4e13b9467e0a450abca6dbb71a31b5d9df9d
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2022-06-16 20:53:12 +0700

    challenge-4

---
+++ belajar-js/index4.js
@@ -0,0 +1,16 @@
+let myArray = ['Cokelat', 45.2, 22, true, 'Programming']
+console.log(myArray)
+
+//Menambahkan array dari belakang
+//Push
+myArray.push("Dimas")
+console.log(myArray)
+//Unshift
+myArray.unshift("Khosyi")
+console.log(myArray)
+
+//Menghapus data 
+//Pop dan shift
+myArray.pop()
+myArray.shift()
+console.log(myArray)

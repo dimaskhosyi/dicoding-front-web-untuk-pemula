7d691e28588a6988550179b18a20f35dc4d93da7
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2022-06-16 18:11:11 +0700

    Challenge 1

---
+++ belajar-js/index.js
@@ -0,0 +1,47 @@
+//Tipe Data pada JavaScript
+console.log('---------------Variabel Tipe Data---------------')
+//Undefined
+let x;
+console.log(typeof(x))
+
+//Numbers
+let y = 10;
+console.log(typeof(y))
+
+let z = 23.33;
+console.log(typeof(z))
+
+	//Kita bisa menambahkan tipe data number
+	let a = 12;
+	let b = 9;
+
+	console.log(a + b)
+	console.log(a - b)
+	console.log(a * b)
+	console.log(a / b)
+	console.log(a % b)
+
+	//Increment Decrement
+	let c = 5;
+	console.log(c++)
+	console.log(c)
+
+	let d = 5
+	console.log(++d)
+
+//Strings
+let greet = 'Hello';
+console.log(typeof(greet))
+
+//Boolean
+let e = true;
+let f = false;
+
+console.log(e)
+console.log(f)
+
+//Null
+let dataNanti = null;
+console.log(dataNanti)
+
+console.log('---------------Variabel Tipe Data---------------')
---
+++ challenge-1/challenge-1.js
@@ -0,0 +1,23 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+let firstName = 'Dimas';
+let lastName = 'Khosyi Triangga';
+let age = 23;
+let isMarried = false;
+
+// TODO
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
